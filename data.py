27e927946e9ad167da4609aac8fdbe1b0fd463a9
renaudatascience <85571576+renauddahou@users.noreply.github.com>
2021-07-23 15:25:53 +0200

    Update data.py

--- data.py
+++ data.py
@@ -172,5 +172,5 @@ data = [["Salut",0],["Bonjour",0],["Bonsoire",0],["Hello",0],["coucou",0],["ça 
 ["/Pobligations",169],
 ["/Pinterdictions",170],
 ["/Psecours",171],
-["cas blessure simple",172],["cas blessures simple",172],["cas blessure simple",172],["cas plaie simple",172], ["blessure plaie",172],
-["cas blessures graves",173],["cas blessure grave",173],["cas blessures grave",173],["cas plaie grave",173], ["blessure plaie grave",173]]
+["cas blessure simple",172],["cas blessures simple",172],["cas blessure simple",172],["cas plaie simple",172], ["blessure plaie",172],["blessure",172],["blessures",172],["Blessure",172],["Blessures",172],["Blessure",172],["Blessures",172],["plaies",172],["plaie",172],["Plaies",172],["Plaie",172],
+["cas blessures graves",173],["cas blessure grave",173],["cas blessures grave",173],["cas plaie grave",173], ["blessure plaie grave",173],["blessures",173],["Blessure",173],["Blessures",173],["plaies",173],["plaie",173],["Plaies",173],["Plaie",173]]
