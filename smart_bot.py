088921daab91fdb6d5a488eb778ecd7e983568b4
renaudatascience <85571576+renauddahou@users.noreply.github.com>
2021-07-24 13:28:58 +0200

    Update smart_bot.py

--- smart_bot.py
+++ smart_bot.py
@@ -52,8 +52,9 @@ def get_intent(replica):
 def get_answer_by_intent(intent):
     if intent in BOT_CONFIG['intents']:
         responses = BOT_CONFIG['intents'][intent]['responses']
-        if responses:
-            return random.choice(responses)
+        return responses
+        #if responses:
+            #return random.choice(responses)
 
 
 
